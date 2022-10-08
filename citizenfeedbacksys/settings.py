1bb3ae66114cf21a8ad666d545eade9860b29150
Piyush789123 <115110560+Piyush789123@users.noreply.github.com>
2022-10-08 06:37:31 +0530

    done

--- citizenfeedbacksys/settings.py
+++ citizenfeedbacksys/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -126,3 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 FROM_EMAIL = "nttrohit@gmail.com"
 EMAIL_PASSWORD = "edwqokxpbaexsctd"
 LOGIN_URL = 'custom_login'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
